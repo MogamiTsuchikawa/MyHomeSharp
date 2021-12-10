7708efe995fdde02be611c971e933fdd5a2ad869
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2021-12-10 23:10:22 +0900

    Lチカプログラムを作成

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-
+using System;
+using RaspberryPi;
+using System.Threading;
 class Program
 {
     static void Main(string[] args)
@@ -6,7 +8,14 @@ class Program
         var builder = WebApplication.CreateBuilder(args);
         var app = builder.Build();
         app.MapGet("/", () => "Hello World!");
-        app.Run();
+        //app.Run();
+        var gpio = new GPIO(true);
+        using GPIO.Pin greenLed = new(gpio, 21, GPIO.Pin.Kind.Output);
+        for(int i=0; i<300; i++)
+        {
+            greenLed.value = !greenLed.value;
+            Thread.Sleep(500);
+        }
 
     }
 }
