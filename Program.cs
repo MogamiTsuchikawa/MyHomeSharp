2c33e5d5a234cd563fc794cd8c9e086f12a89aa2
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2021-12-10 23:55:24 +0900

    テストプログラムを修正

--- Program.cs
+++ Program.cs
@@ -5,12 +5,17 @@ class Program
 {
     static void Main(string[] args)
     {
+        var gpio = new GPIO(true);
+        using GPIO.Pin greenLed = new(gpio, 21, GPIO.Pin.Kind.Output);
         var builder = WebApplication.CreateBuilder(args);
         var app = builder.Build();
         app.MapGet("/", () => "Hello World!");
-        //app.Run();
-        var gpio = new GPIO(true);
-        using GPIO.Pin greenLed = new(gpio, 21, GPIO.Pin.Kind.Output);
+        app.MapGet("/green", ()=>{
+            greenLed.value = !greenLed.value;
+            return greenLed.value.ToString();
+        });
+        app.Run();
+        
         for(int i=0; i<300; i++)
         {
             greenLed.value = !greenLed.value;
