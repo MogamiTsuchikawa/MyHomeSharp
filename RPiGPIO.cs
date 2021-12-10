b46ede95eb2bec890c23c7ac9cb52711f9547ef1
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2021-12-10 23:10:08 +0900

    GPIO制御クラスを作成

--- RPiGPIO.cs
+++ RPiGPIO.cs
@@ -1,40 +1,88 @@
 using System;
+using System.IO;
 
 namespace RaspberryPi
 {
     public class GPIO
     {
-        private const string basePath = "/sys/class/gpio/";
+        private const string BasePath = "/sys/class/gpio/";
         private bool isDebugMode = false;
+        private const int GpioNum = 28;
+        private bool?[] debugValue = new bool?[GpioNum];
+        private Pin[] pins = new Pin[GpioNum];
+
 
         public GPIO(bool isDebugMode)
         {
             this.isDebugMode = isDebugMode;
         }
+        protected string getValuePath(int number) => $"{BasePath}gpio{number}/value";
+        protected string getDirectionPath(int number) => $"{BasePath}gpio{number}/direction";
+        protected string getUnExportPath() => $"{BasePath}unexport";
 
-        public void Write(Pin pin, bool value)
-        {
-            pin.Write();
-        }
-
-        public class Pin
+        public class Pin : IDisposable
         {
             public enum Kind
             {
-                Input = "in", Output = "out"
+                Input, Output
             }
+            string GetKindText() => kind == Kind.Input ? "in" : "out";
 
             public Kind kind { get; private set; }
             public int number { get; private set; }
-            public Pin(int number, Kind kind)
+            private GPIO gpio { get; set; }
+            public Pin(GPIO gpio, int number, Kind kind)
             {
+                this.gpio = gpio;
                 this.number = number;
                 this.kind = kind;
+                if (gpio.isDebugMode)
+                {
+                    Console.WriteLine($"gpio{number} is set {GetKindText()}put mode");
+                    gpio.debugValue[number - 1] = false;
+                }
+                else
+                {
+                    File.WriteAllText(gpio.getDirectionPath(number), GetKindText());
+                }
             }
-            protected void Write(bool value)
+            ~Pin()
             {
-
-            } 
+                if (gpio.isDebugMode)
+                {
+                    Console.WriteLine($"gpio{number} is clear");
+                    gpio.debugValue[number - 1] = null;
+                }
+                else
+                {
+                    File.WriteAllText(gpio.getUnExportPath(), number.ToString());
+                }
+            }
+            void IDisposable.Dispose()
+            {
+                if (gpio.isDebugMode)
+                {
+                    Console.WriteLine($"gpio{number} is clear");
+                    gpio.debugValue[number - 1] = null;
+                }
+                else
+                {
+                    File.WriteAllText(gpio.getUnExportPath(), number.ToString());
+                }
+            }
+            public bool value
+            {
+                get => gpio.isDebugMode ? gpio.debugValue[number - 1] == true : File.ReadAllText(gpio.getValuePath(number)) == "1";
+                set
+                {
+                    if (gpio.isDebugMode) 
+                    {
+                        gpio.debugValue[number - 1] = value;
+                        Console.WriteLine($"gpio{number} is {value.ToString()}");
+                    }
+                    else File.WriteAllText(gpio.getValuePath(number), value ? "1" : "0");
+                }
+            }
         }
     }
 }
