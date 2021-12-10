5b821d7b634b00803c82d47216bc2617dc4b1826
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2021-12-11 01:34:10 +0900

    comeGETを追加

--- Program.cs
+++ Program.cs
@@ -3,24 +3,35 @@ using RaspberryPi;
 using System.Threading;
 class Program
 {
+    static Timer stopBtnTimer;
     static void Main(string[] args)
     {
         var gpio = new GPIO(true);
         using GPIO.Pin greenLed = new(gpio, 21, GPIO.Pin.Kind.Output);
+        using GPIO.Pin stopBtn = new(gpio, 20, GPIO.Pin.Kind.Input);
         var builder = WebApplication.CreateBuilder(args);
         var app = builder.Build();
         app.MapGet("/", () => "Hello World!");
+        TimerCallback stopBtnCheck = state =>
+        {
+            if(!stopBtn.value){
+                greenLed.value = false;
+                stopBtnTimer.Change(Timeout.Infinite, Timeout.Infinite);
+            }
+        };
+        stopBtnTimer = new Timer(stopBtnCheck, null, Timeout.Infinite, Timeout.Infinite);
+
         app.MapGet("/green", ()=>{
             greenLed.value = !greenLed.value;
             return greenLed.value.ToString();
         });
+        app.MapGet("/come", ()=>{
+            greenLed.value = true;
+            stopBtnTimer.Change(3, 3);
+            return "ok";
+        });
         app.Run();
         
-        for(int i=0; i<300; i++)
-        {
-            greenLed.value = !greenLed.value;
-            Thread.Sleep(500);
-        }
 
     }
 }
